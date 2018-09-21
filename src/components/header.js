b22aab56822c08735784b47ccc9068f0ebd74b01
davidkvara <davekvaratskhelia@gmail.com>
2018-09-21 22:52:27 +0400

    tweaks

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "gatsby";
 
 const Header = ({ siteTitle }) => (
-  <header className="pa3">
-    <div className="mw7 center pb3 bb b--black-10">
+  <header className="pa4">
+    <div className="mw7 center pb2">
       <h1 className="ma0 tc dejavu">
         <Link to="/">დათოს ბლოგი</Link>
       </h1>
