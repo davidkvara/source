39f7c3118819368041d5e6287a1f9d1535fa4069
davidkvara <davekvaratskhelia@gmail.com>
2018-09-26 23:50:14 +0400

    new title

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "gatsby";
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <header className="pa4">
-    <div className="mw7 center pb2">
-      <h1 className="ma0 tc dejavu">
-        <Link to="/">დათოს ბლოგი</Link>
+    <div className="mw7 tc center pb2">
+      <h1 className="ma0 f3 dejavu">
+        <Link to="/">დავით კვარაცხელია</Link>
       </h1>
+      <p className="geo-text gray">["აღწერა"]</p>
     </div>
   </header>
 );
+
 export default Header;
